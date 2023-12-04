5681313aad563683f34df1017a50f242584eb5ce
samryanruiz <samryanruiz@gmail.com>
2023-12-04 21:25:10 +0800

    Change the color of the Map Lines

--- components/Map.js
+++ components/Map.js
@@ -61,7 +61,7 @@ const Map = () => {
           destination={destination.description}
           apikey={GOOGLE_MAPS_APIKEY}
           strokeWidth={3}
-          strokeColor="black"
+          strokeColor="#5FC0DE"
           onReady={fitMarkers}
         />
       )}
