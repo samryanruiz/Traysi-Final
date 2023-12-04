1d70342d8d471b78093e50e409cb5ea740f221ba
samryanruiz <samryanruiz@gmail.com>
2023-12-04 21:09:39 +0800

    Modified Ride And Navigate Card

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -106,21 +106,6 @@ const NavigateCard = () => {
 
         <NavFavourites />
       </View>
-
-      {/* Ride and Eats buttons */}
-      <View
-        style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}
-      >
-        <TouchableOpacity
-          onPress={() => navigation.navigate("RideOptionsCard")}
-          style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
-        >
-          <Icon name="car" type="font-awesome" color="white" size={16} />
-          <Text style={tw`text-white text-center`}>Rides</Text>
-        </TouchableOpacity>
-
-        {}
-      </View>
     </SafeAreaView>
   );
 };
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -7,36 +7,46 @@ import {
   TouchableOpacity,
   FlatList,
   Image,
+  Switch,
 } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
-import { useSelector, } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectTravelTimeInformation } from "../slices/navSlice";
 
 const data = [
   {
     id: "Tricycle-Regular-001",
     title: "Regular",
-    multiplier: 1,
     image: "https://www.iconbolt.com/preview/facebook/ionicons-regular/person.svg",
   },
   {
-    id: "Tricycle-Student-002",
+    id: "Tricycle-Special-002",
     title: "Special",
-    multiplier: 0.8,
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRllY2G-xeoq0qSoTn-fbF-w03fInQPk2vD5Wlt-Zvh-ubKmue631hbP38T6mgN-jOBgUw&usqp=CAU",
   },
-  
 ];
-
-// If we have SURGE pricing, this goes up
-const SURGE_CHARGE_RATE = 20;
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+  const [isDiscounted, setIsDiscounted] = useState(false);
   const travelTimeInformation = useSelector(selectTravelTimeInformation);
+
+  const calculateFare = (title, distanceInMeters, isDiscounted) => {
+    const distanceInKilometers = distanceInMeters / 1000;
+    let additionalDistance = distanceInKilometers - 1;
+    if (additionalDistance < 0) additionalDistance = 0;
+
+    let fare = 0;
+    if (title === "Regular") {
+      fare = 10 + additionalDistance * 2;
+    } else if (title === "Special") {
+      fare = 30 + additionalDistance * 5;
+    }
+    return isDiscounted ? fare * 0.8 : fare; // Apply discount if isDiscounted is true
+  };  
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
@@ -48,51 +58,70 @@ const RideOptionsCard = () => {
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
         <Text style={tw`text-center text-xl flex-grow`}>
-          Select
+          Calculated Fare
         </Text>
         <View style={{ width: 10 }} />
       </View>
-
+      
       <FlatList
         data={data}
         keyExtractor={(item) => item.id}
-        renderItem={({ item: { id, title, multiplier, image }, item }) => (
-          <TouchableOpacity
-            onPress={() => setSelected(item)}
-            style={tw`flex-row justify-between items-center px-10 ${
-              id === selected?.id && "bg-gray-200"
-            }`}
-          >
-            <Image
-              style={{
-                width: 80,
-                height: 80,
-                resizeMode: "contain",
-              }}
-              source={{ uri: image }}
-            />
-            <View style={tw`-ml-6`}>
-              <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>{travelTimeInformation?.duration?.text} Travel Time</Text>
-            </View>
-            <Text style={tw`text-xl`}>
-              {new Intl.NumberFormat("en-gb", {
-                style: "currency",
-                currency: "PHP",
-              }).format(
-                (travelTimeInformation?.duration.value *
-                  SURGE_CHARGE_RATE *
-                  multiplier) /
-                  100
-              )}
-            </Text>
-          </TouchableOpacity>
-        )}
+        extraData={isDiscounted}
+        renderItem={({ item: { id, title, image }, item }) => {
+          const fare = calculateFare(title, travelTimeInformation?.distance?.value || 0, isDiscounted);
+          return (
+            <TouchableOpacity
+              onPress={() => setSelected(item)}
+              style={tw`flex-row justify-between items-center px-10 py-5 ${
+                id === selected?.id && "bg-gray-200"
+              }`}
+            >
+              <Image
+                style={{
+                  width: 50,
+                  height: 50,
+                  resizeMode: "contain",
+                }}
+                source={{ uri: image }}
+              />
+              <View style={tw`-ml-6`}>
+                <Text style={tw`text-xl font-semibold`}>{title}</Text>
+                <Text>{travelTimeInformation?.distance?.text} Travel</Text>
+              </View>
+              <Text style={tw`text-xl`}>
+                {new Intl.NumberFormat("en-gb", {
+                  style: "currency",
+                  currency: "PHP",
+                }).format(fare)}
+              </Text>
+            </TouchableOpacity>
+          );
+        }}
       />
+      <View style={styles.discountContainer}>
+        <Text style={styles.discountText}>Discounted (SC, PWD, Student)</Text>
+        <Switch
+          value={isDiscounted}
+          onValueChange={() => setIsDiscounted(previousState => !previousState)}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default RideOptionsCard;
 
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  discountContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+    borderTopWidth: 1,
+    borderColor: '#ccc',
+  },
+  discountText: {
+    fontSize: 16,
+  },
+});
